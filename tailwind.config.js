caa02ef70dd892496fbc9625f8a1fbbd9ed7d8f5
Vazz2004 <samivazqueles@gmail.com>
2024-06-20 22:23:02 -0500

    🔧  Se configura tailwind y colores que se van usar en la app

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ], theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#871cf8",
+        "backgraund-100":"#1A1A1A",
+        "backgraund-200":"#292929",
+        "backgraund-300":"#404040",
+        "backgraund-400":"#5B5B5B",
+      }
+    },
   },
   plugins: [],
 }
