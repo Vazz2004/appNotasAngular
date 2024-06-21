9382972d26a32faecdd523658b9f4c6520210b07
Vazz2004 <samivazqueles@gmail.com>
2024-06-20 22:22:21 -0500

    🔧 Se configuran rutas de navegacion se deja path por defecto para que sea posible navegar en la vista principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TodooComponent } from './components/todoo/todoo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'todoo' , component: TodooComponent },
+  {path:'**', pathMatch: 'full' , redirectTo: 'todoo'}
+]
+  ;
